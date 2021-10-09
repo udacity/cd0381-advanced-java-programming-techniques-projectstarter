152374ec9160744dbb62b34d842745bd379829ea
Ayush Sharma <ayush.sharma@olx.com>
2021-10-09 23:13:36 +0530

    completed method profiling

--- starter/webcrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilerImpl.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilerImpl.java
@@ -1,11 +1,16 @@
 package com.udacity.webcrawler.profiler;
 
 import javax.inject.Inject;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.lang.reflect.Proxy;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.time.Clock;
 import java.time.ZonedDateTime;
+import java.util.Arrays;
 import java.util.Objects;
 
 import static java.time.format.DateTimeFormatter.RFC_1123_DATE_TIME;
@@ -33,13 +38,35 @@ final class ProfilerImpl implements Profiler {
     //       ProfilingMethodInterceptor and return a dynamic proxy from this method.
     //       See https://docs.oracle.com/javase/10/docs/api/java/lang/reflect/Proxy.html.
 
-    return delegate;
+    if(!doesHaveProfiledMethod(klass)){
+      throw new IllegalArgumentException(klass.getName() + " does not have Profiled Methods.");
+    }
+
+    Object proxy = Proxy.newProxyInstance(
+            ProfilerImpl.class.getClassLoader(),
+            new Class[] {klass},
+            new ProfilingMethodInterceptor(delegate, clock, state));
+
+    return (T) proxy;
+  }
+
+  private <T> boolean doesHaveProfiledMethod(Class<T> klass) {
+    return Arrays
+            .stream(klass.getDeclaredMethods())
+            .anyMatch(m -> m.getAnnotation(Profiled.class) != null);
   }
 
   @Override
   public void writeData(Path path) {
     // TODO: Write the ProfilingState data to the given file path. If a file already exists at that
     //       path, the new data should be appended to the existing file.
+    Objects.requireNonNull(path);
+
+    try(Writer writer = Files.newBufferedWriter(path, StandardOpenOption.APPEND)){
+      writeData(writer);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
   @Override
--- starter/webcrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilingMethodInterceptor.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilingMethodInterceptor.java
@@ -1,8 +1,12 @@
 package com.udacity.webcrawler.profiler;
 
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZonedDateTime;
 import java.util.Objects;
 
 /**
@@ -12,19 +16,42 @@ import java.util.Objects;
 final class ProfilingMethodInterceptor implements InvocationHandler {
 
   private final Clock clock;
+  private final ProfilingState profilingState;
+  private final Object delegate;
 
   // TODO: You will need to add more instance fields and constructor arguments to this class.
-  ProfilingMethodInterceptor(Clock clock) {
+  ProfilingMethodInterceptor(Object delegate, Clock clock, ProfilingState profilingState) {
     this.clock = Objects.requireNonNull(clock);
+    this.profilingState = profilingState;
+    this.delegate = delegate;
   }
 
+
   @Override
-  public Object invoke(Object proxy, Method method, Object[] args) {
+  public Object invoke(Object proxy, Method method, Object[] args) throws Throwable{
     // TODO: This method interceptor should inspect the called method to see if it is a profiled
     //       method. For profiled methods, the interceptor should record the start time, then
     //       invoke the method using the object that is being profiled. Finally, for profiled
     //       methods, the interceptor should record how long the method call took, using the
     //       ProfilingState methods.
-    return null;
+    Object methodResponse = null;
+
+    Boolean isProfiled = method.getAnnotation(Profiled.class) != null;
+
+    Instant instant = clock.instant();
+      try {
+        methodResponse = method.invoke(delegate, args);
+      } catch (IllegalAccessException | InvocationTargetException | IllegalArgumentException e) {
+        throw e.getCause();
+      } finally {
+        if(isProfiled){
+          Duration elapsedDuration = Duration.between(instant, clock.instant());
+          profilingState.record(delegate.getClass(), method, elapsedDuration);
+        }
+      }
+
+    return methodResponse;
   }
+
+
 }
